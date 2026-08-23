084b7d89925ab8e547f7bbdd5d9bf8015bd4a970
Zulzario Zaeri <ytryo789@gmail.com>
2026-08-23 23:30:52 +0800

    remove: lupa

--- resources/views/components/guru-menu.blade.php
+++ resources/views/components/guru-menu.blade.php
@@ -35,12 +35,12 @@ $menuItems = [
         'url'         => route('announcements.index'),
         'icon'        => 'megaphone',
     ],
-    [
-        'title'       => 'Menu Wali Kelas',
-        'description' => 'Kelola dan pantau kelas yang Anda wali-i',
-        'url'         => route('walikelas.index'),
-        'icon'        => 'users-group',
-    ]
+    // [
+    //     'title'       => 'Menu Wali Kelas',
+    //     'description' => 'Kelola dan pantau kelas yang Anda wali-i',
+    //     'url'         => route('walikelas.index'),
+    //     'icon'        => 'users-group',
+    // ]
 ];
 
 if ($user->role === 'GURU' && $user->isWaliKelas()) {
